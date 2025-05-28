3dbfb66dae4baee1249e9d14ca4df05a2e2f6406
West Side <officialdee77@gmail.com>
2025-05-28 13:19:07 +0300

    initial commit

---
+++ gabs.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+gabs = Flask(__name__)
+
+@gabs.route('/',)
+def index():
+    return render_template('index.html')
+
+@gabs.route('/sell')
+def sell():
+   return render_template('sell.html')
+
+@gabs.route('/login')
+def login():
+    return render_template('login.html')
+
+@gabs.route('/toyota')
+def toyota():
+    return render_template('toyota.html')
+
+@gabs.route('/about')
+def about():
+    return render_template ('about.html')
+
+@gabs.route('/account')
+def account():
+    return render_template('account.html')
+
+if __name__ == '__main__':
+    gabs.run(debug=True)
+    
